91dd810f3189222827882e3d6c884750ade223fc
Mikquell <angellomchl@gmail.com>
2023-10-28 10:10:36 +0700

    212102445
    UTS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  nama = 'Michael Angello Oraplean';
+  nim = '212102445';
   title = 'UTS212102445';
 }
